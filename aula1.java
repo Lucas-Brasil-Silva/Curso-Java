097e681fdb6df1e193aff4f69ee1f54700968927
Lucas Brasil Silva <lucasbrasil396@gamil.com>
2025-12-10 23:06:28 -0300

    aual2

--- aula1.java
+++ aula1.java
@@ -2,6 +2,10 @@ public class aula1 {
 
     public static void main(String[] args) {
 
+        //Esse comando gera uma print com uma quebra de linha no final
         System.out.println("Ola mundo!!");
+        
+        //Esse comando gera uma print sem uma quebra de linha no final
+        System.out.print("Ola mundo!!");
     }
 }
