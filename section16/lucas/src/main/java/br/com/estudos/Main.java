9bef4ba20317f6554e84bf868e0b8f926c71790d
Lucas Brasil Silva <lucasbrasil396@gamil.com>
2025-12-31 18:59:48 -0300

    update

--- section16/lucas/src/main/java/br/com/estudos/Main.java
+++ section16/lucas/src/main/java/br/com/estudos/Main.java
@@ -3,7 +3,6 @@ package br.com.estudos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
