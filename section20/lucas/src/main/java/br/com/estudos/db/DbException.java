475e462f4496976dace71dc9b28ade7383967caa
Lucas Brasil Silva <lucasbrasil396@gamil.com>
2026-01-01 23:34:23 -0300

    Creating the connection to the cursojdbc database

---
+++ section20/lucas/src/main/java/br/com/estudos/db/DbException.java
@@ -0,0 +1,10 @@
+package br.com.estudos.db;
+
+public class DbException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1l;
+
+    public DbException(String msg) {
+        super(msg);
+    }
+}
